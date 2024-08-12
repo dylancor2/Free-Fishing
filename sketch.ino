00524c5d3dd156f2fbcaa860d81317d0edb25687
Dylan C <137826466+dylancor2@users.noreply.github.com>
2024-08-12 17:58:37 -0400

    Hour 2

--- sketch.ino
+++ sketch.ino
@@ -10,8 +10,13 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 int start = 34;
 int sel = 35;
-
 int slid = 14;
+
+
+bool beg = true;
+int stage = 0;
+
+bool inMenu = false;
 
 void setup() {
   Serial.begin(9600);
@@ -27,24 +32,146 @@ void setup() {
   display.clearDisplay();
   display.setTextSize(1);
   display.setTextColor(SSD1306_WHITE);
-  menu();
+  beginning();
 }
 
 void loop() {
-  // Button press
-  if(digitalRead(start) == HIGH){
-    Serial.println("Started"); 
+
+    if(digitalRead(start) == HIGH){
+      if(beg && stage == 1){
+        men(1);
+      }
+      else{
+        openMenu();
+      }
     }
-  if(digitalRead(sel) == HIGH){
-    Serial.println("selected");  
+    if(digitalRead(sel) == HIGH){
+      if(beg){
+        men(stage);
+      }
+      
+    delay(100);
   }
-  delay(100);
 
 }
 
-void menu(){
-  display.setCursor(5, 5);
-  display.setTextSize(3);
-  display.println("Free Fishing!");
+void beginning(){
+  display.setCursor(40, 0);
+  display.setTextSize(2);
+  display.println("Free");
+  display.setCursor(23, 20);
+  display.println("Fishing!");
+  display.setCursor(35, 40);
+  display.setTextSize(1);
+  display.println("Press Start");
   display.display();
+  stage = 1;
+}
+
+void men(int s){
+  switch(s){
+    case 1:{
+      display.clearDisplay();
+      display.setCursor(40, 0);
+      display.setTextSize(2);
+      display.println("Use");
+      display.setCursor(23, 20);
+      display.println("Select");
+      display.setCursor(35, 40);
+      display.setTextSize(1);
+      display.println("to continue>");
+      display.display();
+      stage = 2;
+      break;
+    }
+    case 2:{
+      display.clearDisplay();
+      display.setCursor(40, 0);
+      display.setTextSize(2);
+      display.println("Use");
+      display.setCursor(30, 20);
+      display.println("Slide");
+      display.setCursor(10, 40);
+      display.setTextSize(1);
+      display.println("to scroll and reel>");
+      display.display();
+      stage = 3;
+      break;
+    }
+    case 3:{
+      display.clearDisplay();
+      display.setCursor(40, 0);
+      display.setTextSize(2);
+      display.println("Tilt");
+      display.setCursor(5, 20);
+      display.println("Controller");
+      display.setCursor(35, 40);
+      display.setTextSize(1);
+      display.println("to cast rod>");
+      display.display();
+      stage = 4;
+      break;
+    }
+    case 4:{
+      display.clearDisplay();
+      display.setCursor(17, 0);
+      display.setTextSize(2);
+      display.println("Rotate");
+      display.setCursor(17, 20);
+      display.println("Encoder");
+      display.setCursor(5, 40);
+      display.setTextSize(1);
+      display.println("to rotate player>");
+      display.display();
+      stage = 5;
+      break;
+    }
+    case 5:{
+      display.clearDisplay();
+      display.setCursor(40, 0);
+      display.setTextSize(2);
+      display.println("Go");
+      display.setCursor(23, 20);
+      display.println("Fish");
+      display.setCursor(35, 40);
+      display.setTextSize(1);
+      display.println("to get moneyy>");
+      display.display();
+      stage = 6;
+      break;
+    }
+    case 6:{
+      display.clearDisplay();
+      display.setCursor(40, 0);
+      display.setTextSize(2);
+      display.println("Use");
+      display.setCursor(23, 20);
+      display.println("Start");
+      display.setCursor(35, 40);
+      display.setTextSize(1);
+      display.println("to open menu>");
+      display.display();
+      beg = false;
+      break;
+    }
+  }
+}
+
+void openMenu(){
+  inMenu = true;
+  display.clearDisplay();
+  display.setCursor(40, 0);
+  display.setTextSize(1.3);
+  display.println("Menu");
+  display.setCursor(35, 20);
+  display.setTextSize(1);
+  display.println("Go Fish!");
+  display.setCursor(35, 40);
+  display.setTextSize(1);
+  display.println("Shop");
+  display.setCursor(25, 60);
+  display.setTextSize(1);
+  display.println("Gamble >:)");
+  display.display();
+  stage = 1;
 }
